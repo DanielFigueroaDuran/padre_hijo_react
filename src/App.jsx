43e76ea9738976afd36e91058d85f9e3c2fc000e
15819045Daniel <DanielFigueroaDuran>
2023-09-12 18:54:21 +0200

    subiendo

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
     return (
         <>
-            <div>hola</div>
+            <div>hola dddd</div>
         </>
     )
 }
